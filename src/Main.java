0cda9c2bd4d00c18b7650c6f8a1d22cf2f589b1c
Joël Elunda <joel.elunda.01@gmail.com>
2020-05-04 09:15:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+
+
+public class Main {
+
+    public static void main(String ... args) {
+        /*
+        * String[] params = new String[3];
+        *
+        * String params[0] = "something";
+        * String params[1] = "langSource";
+        * String params[2] = "langTarget";
+        *
+        * String textTranslated = Translate.doBackground(params);
+        *
+        * */
+    }
+
+}
